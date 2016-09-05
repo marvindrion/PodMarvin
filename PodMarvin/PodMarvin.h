4fac2452d31cd2a082d9bec3fb040a6ad33c3a3f
ClusterGIS <ClusterGIS@minidecrgis2016.corp.capgemini.com>
2016-09-05 15:02:48 +0200

    Initial Commit of PodMarvin
    Signed-off-by: ClusterGIS <ClusterGIS@minidecrgis2016.corp.capgemini.com>

---
+++ PodMarvin/PodMarvin.h
@@ -0,0 +1,13 @@
+//
+//  PodMarvin.h
+//  PodMarvin
+//
+//  Created by ClusterGIS on 05/09/2016.
+//  Copyright © 2016 ClusterGIS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PodMarvin : NSObject
+
+@end
---
+++ PodMarvin/PodMarvin.m
@@ -0,0 +1,16 @@
+//
+//  PodMarvin.m
+//  PodMarvin
+//
+//  Created by ClusterGIS on 05/09/2016.
+//  Copyright © 2016 ClusterGIS. All rights reserved.
+//
+
+#import "PodMarvin.h"
+
+@implementation PodMarvin
+
+-(void)helloWorld{
+    NSLog(@"helloWorld PodMarvin :) ");
+}
+@end
